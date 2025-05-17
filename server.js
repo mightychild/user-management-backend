73cd1d76040836e48afc3500757c79b2610fe9a6
mightychild <69825325+mightychild@users.noreply.github.com>
2025-05-17 11:31:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-// Import route
+// Import routes
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
 
